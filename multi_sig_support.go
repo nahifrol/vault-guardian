4c53ab6b9af6486bc22325c63ec8d83c49143339
nahifrol <sestruha1@outlook.com>
2023-02-18 14:00:00 +0200

    Integrate IPFS for storage

--- multi_sig_support.go
+++ multi_sig_support.go
@@ -1,3 +1,4 @@
 // Fix oracle data delays in decentralized apps
 // Fix deserialization bug in Haskell code
 // Refactor wallet interaction logic
+// Integrate decentralized anonymous identity systems
