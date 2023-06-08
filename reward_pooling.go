f27c943b01735f170d363f9d05f2de219a6d68d9
nahifrol <sestruha1@outlook.com>
2023-06-08 14:00:00 +0200

    Enhance contract event handling

--- reward_pooling.go
+++ reward_pooling.go
@@ -1,3 +1,4 @@
 // Enhance smart contract state changes monitoring
 // Improve contract execution performance
 // Refactor storage layout for better efficiency
+// Add governance proposal expiration feature
