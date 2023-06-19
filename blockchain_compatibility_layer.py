1b611d63c477733af50b24f4674f7044a91a0b19
nahifrol <sestruha1@outlook.com>
2023-06-19 14:00:00 +0200

    Add support for ERC-1155 tokens

--- blockchain_compatibility_layer.py
+++ blockchain_compatibility_layer.py
@@ -1,3 +1,4 @@
 // Improve staking logic for better rewards
 // Add support for decentralized content distribution
 // Optimize decentralized oracle performance
+// Optimize gas usage for contract deployment
