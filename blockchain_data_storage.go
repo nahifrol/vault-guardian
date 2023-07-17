57d74452e431778d76e51d741608b8448730c94e
nahifrol <sestruha1@outlook.com>
2023-07-17 14:00:00 +0200

    Fix ERC-20 token interface bugs

--- blockchain_data_storage.go
+++ blockchain_data_storage.go
@@ -1,3 +1,4 @@
 // Implement on-chain identity verification
 // Improve multi-chain transaction processing
 // Fix issue with multi-chain transactions
+// Improve decentralized lending contract flexibility
