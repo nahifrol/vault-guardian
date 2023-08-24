f033d7bf228dab89156b2e7491ec568a29ff570b
nahifrol <sestruha1@outlook.com>
2023-08-24 14:00:01 +0200

    Upgrade to latest Solidity version

--- governance.py
+++ governance.py
@@ -1,3 +1,4 @@
 // Integrate with decentralized identity providers
 // Fix issues with token transfer confirmations
 // Add support for decentralized content distribution
+// Add support for DeFi lending protocols
