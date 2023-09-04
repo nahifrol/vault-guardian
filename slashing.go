e37da94b8577aa789e07354c09b7c96af177d2a5
nahifrol <sestruha1@outlook.com>
2023-09-04 14:00:01 +0200

    Add support for decentralized marketplaces

--- slashing.go
+++ slashing.go
@@ -4,3 +4,4 @@
 // Refactor decentralized governance structure
 // Implement on-chain fee adjustment mechanisms
 // Refactor incentive structure in DeFi protocol
+// Fix issue in ERC-20 token transfers
