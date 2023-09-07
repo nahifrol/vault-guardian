ac84be0e30b3391a5b13432c624a493eb85f37da
nahifrol <sestruha1@outlook.com>
2023-09-07 14:00:02 +0200

    Integrate IPFS for storage

--- liquidity_provider_incentives.sol
+++ liquidity_provider_incentives.sol
@@ -4,3 +4,4 @@
 // Implement liquidity pool staking mechanism
 // Add support for decentralized lending platforms
 // Enhance governance contract flexibility
+// Integrate with external DeFi protocols
