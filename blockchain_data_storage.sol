6655a7aea191eac17b1ab1109130c3a22552e686
nahifrol <sestruha1@outlook.com>
2023-10-22 14:00:00 +0200

    Improve on-chain governance systems

--- blockchain_data_storage.sol
+++ blockchain_data_storage.sol
@@ -1,3 +1,4 @@
 // Fix deserialization bug in Haskell code
 // Refactor multi-asset token management
 // Fix security vulnerability in random number generation
+// Fix bugs in decentralized exchange routing
