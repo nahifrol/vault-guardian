16269ab7559e875643b7ec3239288d492c10f2fd
nahifrol <sestruha1@outlook.com>
2023-11-11 14:00:01 +0200

    Update testing framework for Solidity

--- emergency_stop.go
+++ emergency_stop.go
@@ -3,3 +3,4 @@
 // Add slashing mechanism for malicious actors
 // Fix edge case in unstaking logic
 // Fix security vulnerability in random number generation
+// Fix issue with multi-chain transactions
