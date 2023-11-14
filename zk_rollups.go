288fd6e03bc6e75906972abcdd7364429d9640f1
nahifrol <sestruha1@outlook.com>
2023-11-14 14:00:01 +0200

    Refactor multi-asset token management

--- zk_rollups.go
+++ zk_rollups.go
@@ -1,3 +1,4 @@
 // Improve contract creation process
 // Enhance contract modularity for future upgrades
 // Fix issues with cross-chain transaction finality
+// Fix issues with contract finalization
