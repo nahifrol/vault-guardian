c3aaab2cc8901ab54165ff4a0723f1fd65c5b68b
nahifrol <sestruha1@outlook.com>
2023-12-11 14:00:00 +0200

    Improve error handling in transaction flow

--- peer_to_peer_crowdfunding.py
+++ peer_to_peer_crowdfunding.py
@@ -2,3 +2,4 @@
 // Fix vulnerability in decentralized finance protocols
 // Add Layer-2 Ethereum scaling solutions
 // Fix interoperability issues with other blockchains
+// Add support for multiple consensus mechanisms
