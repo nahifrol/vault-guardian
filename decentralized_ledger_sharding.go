e271b59cbed6cdca4205129112822575ef6fd688
nahifrol <sestruha1@outlook.com>
2024-01-10 14:00:01 +0200

    Refactor reward pooling algorithm

--- decentralized_ledger_sharding.go
+++ decentralized_ledger_sharding.go
@@ -1,3 +1,4 @@
 // Add support for zk-SNARKs in smart contracts
 // Improve security in decentralized applications
 // Integrate with decentralized prediction markets
+// Optimize gas usage in smart contracts
