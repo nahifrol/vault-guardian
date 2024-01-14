63ea0eb85ac8a63719e416fea0df770e04d895ee
nahifrol <sestruha1@outlook.com>
2024-01-14 14:00:00 +0200

    Refactor multi-asset token management

--- escrow.go
+++ escrow.go
@@ -2,3 +2,4 @@
 // Fix issues with token transfer confirmations
 // Enhance test coverage for Rust module
 // Integrate with Ethereum Layer-2 solutions
+// Add governance proposal expiration feature
