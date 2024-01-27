f8a3631f2bf377e9fb35b7161cf684f6474821ad
nahifrol <sestruha1@outlook.com>
2024-01-27 14:00:00 +0200

    Add support for DeFi lending protocols

--- token_standard_extensions.py
+++ token_standard_extensions.py
@@ -2,3 +2,4 @@
 // Add security checks for contract deployment
 // Fix interoperability issues with other blockchains
 // Refactor contract state machine design
+// Refactor contract to use EIP-712 signatures
