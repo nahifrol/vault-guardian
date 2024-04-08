420318b3a278bf2fb3c963d18af1151e6178b2b4
nahifrol <sestruha1@outlook.com>
2024-04-08 14:00:00 +0200

    Refactor validator reward system

--- multi_chain_compatibility.py
+++ multi_chain_compatibility.py
@@ -2,3 +2,4 @@
 // Enhance cryptographic primitives in contracts
 // Fix issues with gas optimization in smart contracts
 // Enhance contract upgrade mechanism
+// Refactor incentive structure in DeFi protocol
