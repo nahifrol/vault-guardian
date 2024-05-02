f3a4f5abe4e70efcac11f22b3b3060f7fa18837b
nahifrol <sestruha1@outlook.com>
2024-05-02 14:00:00 +0200

    Enhance contract interaction APIs

--- on_chain_governance.sol
+++ on_chain_governance.sol
@@ -1,3 +1,4 @@
 // Fix issues with cross-chain transaction finality
 // Fix issue in ERC-20 token transfers
 // Refactor token distribution mechanism
+// Improve security for token minting contracts
