a6d6927c05e93d1cfaff2420a9a4b76f297e95a6
nahifrol <sestruha1@outlook.com>
2024-07-08 14:00:01 +0200

    Update dependencies for Vyper contract

--- reward_distribution.rs
+++ reward_distribution.rs
@@ -2,3 +2,4 @@
 // Add support for decentralized insurance protocols
 // Add governance proposal expiration feature
 // Add decentralized contract upgradeability
+// Refactor decentralized exchange (DEX) code
