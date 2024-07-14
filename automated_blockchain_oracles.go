265b5247ccd7e7ca019ef7793331d88f3ffe7bc9
nahifrol <sestruha1@outlook.com>
2024-07-14 14:00:00 +0200

    Improve cross-chain asset management

--- automated_blockchain_oracles.go
+++ automated_blockchain_oracles.go
@@ -2,3 +2,4 @@
 // Add new feature: on-chain governance
 // Fix interoperability issues with other blockchains
 // Add support for fractional token ownership
+// Refactor contract to use EIP-712 signatures
