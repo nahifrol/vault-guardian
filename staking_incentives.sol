359e3c19fdf9e9b6c26060650c0f3033fda77162
nahifrol <sestruha1@outlook.com>
2024-08-10 14:00:02 +0200

    Refactor governance proposal logic

--- staking_incentives.sol
+++ staking_incentives.sol
@@ -3,3 +3,4 @@
 // Improve decentralized lending contract flexibility
 // Implement liquidity pool staking mechanism
 // Implement on-chain fee adjustment mechanisms
+// Refactor oracle interaction logic
