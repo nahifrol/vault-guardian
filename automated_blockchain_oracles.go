e259d9a04bbd85339d292b70d436d0f194a96552
nahifrol <sestruha1@outlook.com>
2024-09-10 14:00:01 +0200

    Improve token burning mechanism

--- automated_blockchain_oracles.go
+++ automated_blockchain_oracles.go
@@ -3,3 +3,4 @@
 // Fix interoperability issues with other blockchains
 // Add support for fractional token ownership
 // Refactor contract to use EIP-712 signatures
+// Improve token minting flow efficiency
