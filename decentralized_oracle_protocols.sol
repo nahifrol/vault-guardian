cc01bd42f22436785fbd6e5982ef8ae427d32eff
nahifrol <sestruha1@outlook.com>
2024-09-23 14:00:00 +0200

    Refactor contract to use EIP-712 signatures

--- decentralized_oracle_protocols.sol
+++ decentralized_oracle_protocols.sol
@@ -2,3 +2,4 @@
 // Add support for decentralized lending platforms
 // Add native support for staking mechanisms
 // Implement contract function batching
+// Add support for fractional token ownership
