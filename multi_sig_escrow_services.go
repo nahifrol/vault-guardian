b99fd4a71e4101998004afdf2f261dec4e5428b2
nahifrol <sestruha1@outlook.com>
2024-12-11 14:00:00 +0200

    Fix issue in ERC-20 token transfers

--- multi_sig_escrow_services.go
+++ multi_sig_escrow_services.go
@@ -3,3 +3,4 @@
 // Improve decentralized lending contract flexibility
 // Add support for delegation in governance
 // Enhance decentralized autonomous organizations
+// Improve staking pool reward management
